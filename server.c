eddfabe77d0f9f28c10bb98a5223e88527d78e75
Frengki1020 <if313008@gmail.com>
2021-04-07 13:59:23 +0700

    Update server.c

--- server.c
+++ server.c
@@ -9,12 +9,12 @@
 #include <unistd.h>
 
 #define ALL_SIZE 8192
+#define NET_BUF_SIZE 2048+1
+#define FILE_BUFF  8192
 
 
 #define IP_PROTOCOL 0
 #define PORT_NO 15050
-#define NET_BUF_SIZE 2048+1
-#define FILE_BUFF 8192
 #define BUFF 1
 #define cipherKey 'S'
 #define sendrecvflag 0
@@ -47,6 +47,7 @@ int main(){
     	char sendbuff[BUFF];
     	/**/
     	struct frame_t frame;
+    	struct frame_t temp_frame;
     	struct stat st;
     	struct timeval t_out = {0, 0};
     	FILE *fp, *file_binary;
@@ -56,33 +57,31 @@ int main(){
     	/*Membuat Socket*/
     	sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL);
     	if (sockfd < 0)
-        	printf("\nfile descriptor not received!!\n");
+        	printf("file descriptor not received!!\n");
     	else
-        	printf("\nfile descriptor %d received\n", sockfd);
+        	printf("file descriptor %d received\n", sockfd);
     	/*Membuat Binding*/
     	if (bind(sockfd, (struct sockaddr *)&addr_con, sizeof(addr_con)) == 0)
-        	printf("\nSuccessfully binded!\n");
+        	printf("Successfully binded!\n");
     	else
-        	printf("\nBinding Failed!\n");
+        	printf("Binding Failed!\n");
     	/*Loop*/
     	while (1){
         	printf("Waiting for file name...\n");
         	clearBuf(net_buf);
         	/*menerima file yang telah di-entry di client*/
         	nBytes = recvfrom(sockfd, net_buf,NET_BUF_SIZE, sendrecvflag, (struct sockaddr *)&addr_con, &addrlen);
-        	printf("\nFile Name Received: %s\n", net_buf);
+        	printf("File Name Received: %s\n", net_buf);
         	char find_file[100] = "";
         	/**/
         	strcat(find_file, net_buf);
-        	printf("%s\n", find_file);
-        	
+        	printf("Nama File txt: %s\n", find_file);
         	fp = fopen(find_file, "r");
-        	
         	long int file_size = 0, total_frame = 0;
         	int file_found = 0;
         	/*Kondisi File tidak ditemukan*/
         	if (fp == NULL){
-            		printf("\nFile open failed!\n");
+            		printf("File open failed!\n");
             		sendto(sockfd, &file_found, sizeof(int), sendrecvflag, (struct sockaddr *)&addr_con, addrlen);
         	}
         	/*Kondisi File ada*/
@@ -90,7 +89,7 @@ int main(){
             		file_found = 1;
             		//kirim status file found ke client
             		sendto(sockfd, &file_found, sizeof(int), sendrecvflag, (struct sockaddr *)&addr_con, addrlen);
-            		printf("\nFile Successfully opened!\n");
+            		printf("File Successfully opened!\n");
             		char bin_file[100] = "";
             		/*Menambahkan String dari net_buf ke bin_file*/
             		strcat(bin_file, net_buf);
@@ -98,7 +97,7 @@ int main(){
             		file_binary = fopen(bin_file, "r");
             		/**/
             		if (file_binary == NULL){
-                		printf("\nBinary file is not existed! Creating process ...\n");
+                		//printf("Binary file is not existed! Creating process ...\n");
                 		char command[200] = "xxd -b -c1 ";
                 		strcat(command, find_file);
                 		strcat(command, " | cut -d\" \" -f2 | tr -d \"\\n\" > ");
@@ -117,13 +116,9 @@ int main(){
             		/**/
             		stat(bin_file, &st);
             		file_size = st.st_size;
-			
-			
-			printf("%ld\n",file_size);
-			
-			
-			
-            		t_out.tv_sec = 2;
+			//printf("%ld\n",file_size);
+            		printf("Data Biner : %ld Bytes\n", file_size);
+			t_out.tv_sec = 2;
             		t_out.tv_usec = 0;
             		/**/
             		setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *)&t_out, sizeof(struct timeval));
@@ -137,7 +132,7 @@ int main(){
             		sendto(sockfd, &total_frame, sizeof(long int), sendrecvflag, (struct sockaddr *)&addr_con, addrlen);
             		//menerima ack number dari client
             		recvfrom(sockfd, &(ack_num), sizeof(ack_num), 0, (struct sockaddr *)&addr_con, &addrlen);
-            		printf("ACK Number : %ld\n", ack_num);
+            		printf("\n ACK Number : %ld\n", ack_num);
             		/*kondisi jika ack number yg dikirim tidak sesuai dengan yg diterima*/
             		while (ack_num != total_frame){
             			/**/
@@ -159,26 +154,30 @@ int main(){
                 		sendFile(file_binary, net_buf, frame.data, FILE_BUFF);
                 		//kirim file binary ke client
                 		sendto(sockfd, &frame, sizeof(frame), sendrecvflag, (struct sockaddr *)&addr_con, addrlen);
-                		clearBuf(net_buf);
+                		
+                		
+                		
                 		//menerima ack num dari client
                 		recvfrom(sockfd, &(ack_num), sizeof(ack_num), 0, (struct sockaddr *)&addr_con, &addrlen);
                 		/*kondisi jika terjadi loss connection*/
                 		while (ack_num != frame.id){
                 			/**/
-                    			sendFile(fp, net_buf, frame.data, FILE_BUFF);
+                    			
                     			sendto(sockfd, &frame, sizeof(frame), sendrecvflag, (struct sockaddr *)&addr_con, addrlen);
-                    			clearBuf(net_buf);
+                    			
                     			/**/
                     			recvfrom(sockfd, &(ack_num), sizeof(ack_num), 0, (struct sockaddr *)&addr_con, &addrlen);
-                    			printf("[-] Frame ===> %ld	dropped, %ld times\n", frame.id, ++drop_frame);
+                    			printf("\n [-] Frame ===> %ld	dropped, %ld times\n", frame.id, ++drop_frame);
                     			resend_frame++;
                     			/**/
-                    			printf("[-] Frame ===> %ld	dropped, %ld times\n", frame.id, drop_frame);
-                    			if (resend_frame == 200){
+                    			printf("\n [-] Frame ===> %ld	dropped, %ld times\n", frame.id, drop_frame);
+                    			if (resend_frame == 10){
                         			t_out_flag = 1;
                         			break;
                     			}
                 		}
+                		
+                		clearBuf(net_buf);
                 		resend_frame = 0;
                 		drop_frame = 0;
                 		/**/
@@ -187,15 +186,15 @@ int main(){
                     			printf("File not sent\n");
                     			break;
                 		}
-                		printf(" send ==> %ld\n", ack_num);
-                		
-                		printf(" send ==> %ld\n", frame.id);
-                		
+                		printf("\n send ACK NUMBER==> %ld\n", ack_num);
+                		printf("\n send FRAME ID==> %ld\n", frame.id);
                 		/*Kondisi saat ack_num sudah sama dengan total_frame maka file berhasil dikirim*/
                 		if (ack_num == total_frame){
                     			printf("File sent successfully\n");
                     			fclose(file_binary);
                 		}
+                		
+                		
             		}
             		fclose(fp);
         	}
@@ -237,6 +236,7 @@ int sendFile(FILE *fp, char *buf, char out[ALL_SIZE+1], int s){
 		ch2 = ch;
 		temp[i] = ch2;
 		if (ch == EOF)
+			//temp[i]='\0';
 	    		return 1;
     	}
     	/*menambahkann parity bit di akhir frame bergantung thd All_Size*/
@@ -250,7 +250,8 @@ int sendFile(FILE *fp, char *buf, char out[ALL_SIZE+1], int s){
                 	out[y] = count_parity(temp, ALL_SIZE);
                }
         }
-        printBuff(out);
+        printf("\n");
+        //printBuff(out);
         return 0;
 }
 /*print send frame data ke client*/
